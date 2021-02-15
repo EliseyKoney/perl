11852be020268477f69b61695493a03cd775ab41
Elisey Koney <eliseykoney@gmail.com>
2021-02-15 19:24:54 +0000

    add script 3

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker run --name mysqlperl -e MYSQL_ROOT_PASSWORD=passroot\
-	-e MYSQL_DATABASE=perl\
-	-e MYSQL_USER=mady\
-	-e MYSQL_PASSWORD=passm\
-	-p 3306:3306
- 	-d mysql
+docker run --name mysqlperl -e MYSQL_ROOT_PASSWORD=passroot \                              193ms  Mon Feb 15 19:24:00 2021
+                 -e MYSQL_DATABASE=perl \
+                 -e MYSQL_USER=mady \
+                 -e MYSQL_PASSWORD=passm \
+                 -p 3306:3306 \
+                 -d mysql
