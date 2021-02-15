ed65f3c87b13f1ffeaf6a5025eaaef58ed00ab8e
Elisey Koney <eliseykoney@gmail.com>
2021-02-15 19:58:02 +0000

    add script

--- script.sh
+++ script.sh
@@ -6,3 +6,10 @@ docker run --name mysqlperl -e MYSQL_ROOT_PASSWORD=passroot \
                  -e MYSQL_PASSWORD=passm \
                  -p 3306:3306 \
                  -d mysql
+
+
+
+ALTER USER 'mady'@'localhost' IDENTIFIED WITH mysql_native_password BY 'passm';
+
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'passroot';
+flush privileges;
